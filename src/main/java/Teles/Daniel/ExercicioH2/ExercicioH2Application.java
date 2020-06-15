6909cda468f4324fb31c188a40593511701ccbeb
DanielCesarTeles <daniel.cesarteles@gmail.com>
2020-06-15 17:15:43 -0300

    First commit ExercicioH2ComSpringboot

---
+++ src/main/java/Teles/Daniel/ExercicioH2/ExercicioH2Application.java
@@ -0,0 +1,13 @@
+package Teles.Daniel.ExercicioH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Teles/Daniel/ExercicioH2/ExercicioH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Teles.Daniel.ExercicioH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
